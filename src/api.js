ffc3e00912545950b61767c6ad7e21fee39e4961
Sekgomotso_s <motsoshalang@gmail.com>
2020-03-09 16:56:58 +0200

    body-parser

--- src/api.js
+++ src/api.js
@@ -1,44 +1,57 @@
 "use strict"
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// app set up
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+// parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const Pool = require("pg").Pool;
 
-require('dotenv').config();
-
 const pool = new Pool({
-  user: "user",
-  host: "localhost",
-  database: "prospective_umuzi_students",
-  password: "pass",
-  port: 5432
-});
+    user: "user",
+    host: "localhost",
+    database: "prospective_umuzi_students",
+    password: "pass",
+    port: 5432
+  });
+  
+  pool.connect();
 
-pool.connect();
+// form route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/form.html"));
+  });
 
-app.get('/', (req,res,next)=>{
-    res.addNewVisitor.json({
-        addNewVisitor: "ok"
+app.post('/new_visit', async (req, res) => {
+
+    let visitor_name = req.body.name;
+    let visitors_age = req.body.age;
+    let date_of_visit = req.body.date;
+    let time_of_visit = req.body.time;
+    let nameOfAssistant = req.body.assistant;
+    let comment = req.body.comment;
+
+    const visitor = await addNewVisitor(visitor_name, visitors_age, date_of_visit, time_of_visit, nameOfAssistant, comment)
+
+    res.render('/form', {
+        id: visitor[0].id,
+        visitor_name: req.body.name,
+        visitors_age : req.body.age,
+        visitors_date: req.body.date,
+        time_of_visit: req.body.time,
+        nameOfAssistant: req.body.assistant,
+        comment: req.body.comment
     })
 })
 
-app.post('/', (req, res)=>{
-
-    addNewVisitor(
-         req.body.name,
-         req.body.age,
-         req.body.date,
-         req.body.time,
-         req.body.assistant,
-         req.body.comment
-
-    )
-})
-
-// Save visitor into database
+// create new visitor in the database
 const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => {
 
     try{
@@ -49,13 +62,11 @@ const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => 
   
         return query.rows
   
-    } catch(error) {
-      console.log(error)
+    } catch(err) {
+      console.log(err)
   
     }
 };
-
-addNewVisitor('Jobe', 27, '01/09/2020','10:00','Tumi', 'Lovely.');
 
 app.listen (3005, (req, res) => {
     console.log('server listening in port 3005')
